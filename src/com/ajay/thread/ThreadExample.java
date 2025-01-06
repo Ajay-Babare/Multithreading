8a900958d72b4440794fdb3c30a4647d27851955
Ajay-Babare <ajaybabare97@gmail.com>
2025-01-06 17:57:29 +0530

    Thread State

--- src/com/ajay/thread/ThreadExample.java
+++ src/com/ajay/thread/ThreadExample.java
@@ -1,16 +1,29 @@
 package com.ajay.thread;
+// 3 ways to create the Thread
+// 1. extending Thread class
+// 2. implementing the Runnable interface
+// 3. Implementing the Callable interface
 
+
+// Thread State -> new, runnable, running, sleep, wait, dead
 public class ThreadExample extends Thread{
-	
+	ThreadExample(){
+		super();
+	}
+	ThreadExample(String str){
+		super(str);
+	}
 	public static void main(String[] args) {
-		ThreadExample t1 = new ThreadExample();
-		ThreadExample t2 = new ThreadExample();
-		t1.start();
-		t2.start();
+		ThreadExample t1 = new ThreadExample();//new
+		t1.setName("abc");
+		ThreadExample t2 = new ThreadExample("ThreadName");
+		t1.start(); // It create independent path of execution
+		t2.start(); // runnable
 	}
 	
 	@Override
-	public void run() {
+	public void run() {//running
 		System.out.println("Hi");
+		System.out.println(Thread.currentThread().getName());
 	}
 }
